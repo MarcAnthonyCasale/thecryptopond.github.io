13e44b4d88c8e899ecf508e8ca8982c1d710bfd6
Marc <marc@lilypaddigital.co>
2024-09-27 10:04:10 +0800

    Php mailer setup

--- forms/contact.php
+++ forms/contact.php
@@ -2,36 +2,53 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require '../vendor/autoload.php';
+require 'vendor/autoload.php'; // Autoload PHPMailer
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect form data
+    $twitter = $_POST['question1'];
+    $telegram = $_POST['question2'];
+    $crypto_experience = $_POST['question3'];
+    $meme_experience = $_POST['question4'];
+    $goal = $_POST['question5'];
+    $why_chosen = $_POST['question6'];
+    $groups = $_POST['question7'];
+    $referrals = $_POST['question8'];
 
-  $mail = new PHPMailer(true);
+    $mail = new PHPMailer(true);
+    
+    try {
+        // Server settings
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com'; // Gmail SMTP server
+        $mail->SMTPAuth = true;
+        $mail->Username = 'thecryptopond02@gmail.com'; // Your Gmail address
+        $mail->Password = 'bemc zjjr vnuy bftk'; // Use your App password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
 
-  try {
-      // SMTP settings
-      $mail->isSMTP();
-      $mail->Host = 'smtp.gmail.com';
-      $mail->SMTPAuth = true;
-      $mail->Username = '';
-      $mail->Password = '';     
-      $mail->SMTPSecure = 'tls';            
-      $mail->Port = 587;                    
-  
-      // Sender and recipient settings
-      $mail->setFrom('', '');
-      $mail->addAddress('', ''); 
-  
-      // Email content
-      $mail->isHTML(true);
-      $mail->Subject = 'New Form Submission from your website';
-      $mail->Body    = '<p>This is the body of the email</p>';
-      
-      // Send email
-      $mail->send();
-      echo 'Message has been sent';
-  } catch (Exception $e) {
-      echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-  }
+        // Recipients
+        $mail->setFrom('thecryptopond02@gmail.com', 'The Crypto Pond');
+        $mail->addAddress('marcanthonycasale1@gmail.com', 'Crypto Pond'); // Recipient's email
 
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = 'New Form Submission';
+        $mail->Body = "
+            Twitter Username: $twitter<br>
+            Telegram Username: $telegram<br>
+            Crypto Experience: $crypto_experience<br>
+            Memecoins Experience: $meme_experience<br>
+            Goal: $goal<br>
+            Why Chosen: $why_chosen<br>
+            Groups: $groups<br>
+            Referrals: $referrals
+        ";
 
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+}
 ?>
